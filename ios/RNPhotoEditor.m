15e78fa81eda1aee643ea0c9c5553d01e12e4389
Evan Kristian <evkristian@gmail.com>
2019-04-15 15:39:30 +0700

    update

--- ios/RNPhotoEditor.m
+++ ios/RNPhotoEditor.m
@@ -17,13 +17,13 @@ RCTResponseSenderBlock _onCancelEditing = nil;
 - (void)doneEditingWithImage:(UIImage *)image {
     if (_onDoneEditing == nil) return;
     // Save image.
-//    NSURL *tmpDirURL = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
-//    NSURL *fileURL = [[tmpDirURL URLByAppendingPathComponent:[NSString stringWithFormat:@"%f",[[NSDate date] timeIntervalSince1970]]] URLByAppendingPathExtension:@"jpg"];
-//
-//    [UIImageJPEGRepresentation(image, 0.8) writeToURL:fileURL atomically:YES];
+   NSURL *tmpDirURL = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
+   NSURL *fileURL = [[tmpDirURL URLByAppendingPathComponent:[NSString stringWithFormat:@"%f",[[NSDate date] timeIntervalSince1970]]] URLByAppendingPathExtension:@"jpg"];
+
+   [UIImageJPEGRepresentation(image, 0.8) writeToURL:fileURL atomically:YES];
     
     // Save image.
-    [UIImagePNGRepresentation(image) writeToFile:_editImagePath atomically:YES];
+    // [UIImagePNGRepresentation(image) writeToFile:_editImagePath atomically:YES];
     
     _onDoneEditing(@[[fileURL absoluteString]]);
 }
